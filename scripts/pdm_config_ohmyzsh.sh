74f4e451ecf0eaebdef7eecb0618f6f9b5cc3bb9
Marco André Mendes <marcoandre@gmail.com>
2024-06-27 14:11:24 -0300

    feat: Atualizar script de configuração do pdm para ohmyzsh

--- scripts/pdm_config_ohmyzsh.sh
+++ scripts/pdm_config_ohmyzsh.sh
@@ -5,7 +5,7 @@ pdm completion zsh >> ~/.zshrc
 # se não existe, cria o diretório
 [ ! -d $ZSH_CUSTOM/plugins/pdm ] && mkdir $ZSH_CUSTOM/plugins/pdm
 
-pdm completion zsh > $ZSH_CUSTOM/plugins/pdm/_pdm
+# pdm completion zsh > $ZSH_CUSTOM/plugins/pdm/_pdm
 
 pdm --pep582 zsh > $ZSH_CUSTOM/plugins/pdm/_pdm_pep582
 
